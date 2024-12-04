21a35db4e767e3fa30d0256bfce3ab07bd645c0d
rubensiregar <rubbento88@gmail.com>
2024-12-04 15:26:29 +0700

    init role create

---
+++ services/role.services.ts
@@ -0,0 +1,45 @@
+import { createNewRole } from "../dal/role.dal";
+
+interface IRole {
+    name: string;
+}
+
+const serviceCreateRole = async (data: IRole): Promise<any> => {
+    try {
+        const newUser = await createNewRole;
+        return newUser;
+    } catch (error) {
+        throw error;
+    }
+};
+const serviceGetRole = async (data: IRole): Promise<any> => {
+    try {
+        const newUser = await createNewRole;
+        return newUser;
+    } catch (error) {
+        throw error;
+    }
+};
+const serviceUpdateRole = async (data: IRole): Promise<any> => {
+    try {
+        const newUser = await createNewRole;
+        return newUser;
+    } catch (error) {
+        throw error;
+    }
+};
+const serviceDeleteRole = async (data: IRole): Promise<any> => {
+    try {
+        const newUser = await createNewRole;
+        return newUser;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export {
+    serviceCreateRole,
+    serviceGetRole,
+    serviceUpdateRole,
+    serviceDeleteRole,
+};
